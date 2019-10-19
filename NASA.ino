047fa29d5be5a4fe94ba35c156d5fd0a779817e3
kemy07 <56756552+kemy07@users.noreply.github.com>
2019-10-19 11:28:21 +0200

    Add files via upload

---
+++ NASA.ino
@@ -0,0 +1,17 @@
+int x=0 , w=A5 , b= 2;
+
+
+void setup() {
+  pinMode(2,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+ x = analogRead(w);
+  
+ if (x>=100)
+     digitalWrite(b,HIGH);
+ else 
+     digitalWrite(b,LOW);    
+ 
+ }
